b76fb7e61d80361afc6688c7a1031f32c6bff9b3
zlbao Home <276363733@qq.com>
2019-02-11 06:43:30 +0000

    完成用户注册功能

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,17 @@ class UserController extends Controller
             'email'=>'required|email|unique:users|max:255',
             'password'=>'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password)
+        ]);
+
+        //用戶註冊成功後的信息
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show',[$user]);
+        //相當於redirect()->route('users.show', [$user->id]);
     }
 
 }
