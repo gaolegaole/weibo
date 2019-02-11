d540c274c3bb9b93069e85cf67358e518e1fe21e
zlbao Home <276363733@qq.com>
2019-02-11 01:03:32 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,16 @@
 @section('title','主页')
 
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>hello laravel</h1>
+    <p class="lead">
+      你现在看到的是 <a href="">xxx</a>
+    </p>
+    <p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" role="button" href="#">现在注册</a>
+    </p>
+  </div>
 @stop
