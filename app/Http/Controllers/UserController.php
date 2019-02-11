99a1396d28c1a47ad56a136d368382d726c2d4bb
zlbao Home <276363733@qq.com>
2019-02-11 07:53:31 +0000

    用户登录逻辑

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 class UserController extends Controller
 {
     public function create(){
@@ -27,7 +28,8 @@ class UserController extends Controller
             'email'=>$request->email,
             'password'=>bcrypt($request->password)
         ]);
-
+        //注册成功后自动登录
+        Auth::login($user);
         //用戶註冊成功後的信息
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show',[$user]);
